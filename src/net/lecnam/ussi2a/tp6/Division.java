e11659b4fcee3451312d789f88a99ec9347d8d49
Leroy Ramkalia <leroy.ramkalia.auditeur@lecnam.net>
2021-12-08 10:26:03 +0100

    Exercice 1

---
+++ src/net/lecnam/ussi2a/tp6/Division.java
@@ -0,0 +1,12 @@
+package net.lecnam.ussi2a.tp6;
+
+public class Division {
+
+    public Division(){
+    }
+
+    public double division1(int a, int b){
+        return(a/b);
+    }
+
+}
--- src/net/lecnam/ussi2a/tp6/Main.java
+++ src/net/lecnam/ussi2a/tp6/Main.java
@@ -2,7 +2,13 @@ package net.lecnam.ussi2a.tp6;
 
 public class Main {
 
+    public static void exercice1(int a, int b){
+        Division div = new Division();
+        System.out.println(div.division1(a, b));
+    }
+
     public static void main(String[] args) {
+        exercice1(10, 0);
 
 
     }
